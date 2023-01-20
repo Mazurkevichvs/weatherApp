e34191daa35ce5d8caa5b65e650726e008451bde
Mazurkevichvs <mazurkevichvs@gmail.com>
2023-01-20 20:43:15 +0100

    add error component, set loading state and spinner

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,27 @@ import React, {useState, useEffect} from 'react'
 import './App.scss'
 import Header from './Components/Header'
 import Main from './Components/Main'
+import Error from './Components/Error'
+import { SpinnerCircular } from 'spinners-react';
 
 function App() {
   const [currentWeather, setCurrentWeather] = useState('')
   const [dailyWeather, setDailyWeather] = useState('')
   const [city, setCity] = useState('')
   const [time, setTime] = useState('')  
+  const [incorrectInput, setIncorrectInput] = useState(false)
+  const [error, setError] = useState('')
+  const [isLoading, setIsLoading] = useState(true)
+
+
 
   const getCityName = (cityName) => {
-    setCity(cityName)
+    if(cityName && typeof cityName === "string" && cityName.trim() !== '') {
+      setCity(cityName)
+      setIncorrectInput(false)
+    } else {
+      setIncorrectInput(true)
+    }
   }
 
   useEffect(() => {
@@ -21,29 +33,50 @@ function App() {
     }
     const todayTime = new Date().toLocaleDateString("en-US", options)
     setTime(todayTime)
+    setIsLoading(true)
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city ? city : 'Gdansk'}&appid=c84c59d4d104a2550f66e46573ece2ed&units=metric&lang=en`)
     .then(res => res.json())
     .then(data => {
-      setCurrentWeather(data)
-      
+      if(data.cod === '404') {
+        setIsLoading(false)
+        setError(data.message)
+      } else {
+        setCurrentWeather(data)
+        setIsLoading(false)
+        setError(false)
+      } 
     })
-    .catch(err => console.error(err))
+    .catch(err => {
+      setIsLoading(false)
+      setError(err.message)
+    })
 
     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city ? city : 'Gdansk'}&appid=c84c59d4d104a2550f66e46573ece2ed&units=metric&lang=en`)
     .then(res => res.json())
     .then(data => {
-      setDailyWeather(data)
-      console.log(data) 
+      if(data.cod === '404') {
+        setIsLoading(false)
+        setError(data.message)
+      } else {
+        setDailyWeather(data) 
+        setIsLoading(false)
+        setError(false)
+      }
     })
-    .catch(err => console.error(err))
+    .catch(err => {
+      setIsLoading(false)
+      setError(err.message)
+    })
 
   },[city])
 
   return (
     <div className="App">
-      <div className={currentWeather ? "container" : 'container  loading'}>
-      <Header getCityName={getCityName}/>
-      {currentWeather && <Main time={time} currentWeather={currentWeather} dailyWeather={dailyWeather}/>}
+      <div className='container'>
+      <Header getCityName={getCityName} incorrectInput={incorrectInput}/>
+      {isLoading && <SpinnerCircular size={90} thickness={100} speed={100} color="rgba(255, 255, 255, 1)" secondaryColor="rgba(0, 0, 0, 0.39)" />}
+      {dailyWeather && !error && <Main time={time} currentWeather={currentWeather} dailyWeather={dailyWeather}/>}
+      {error && <Error error={error}/>}
       </div>
     </div>
   )
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,22 @@
 import React, { useState } from 'react'
 import './Header.scss'
 
-const Header = ({getCityName}) => {
+const Header = ({getCityName, incorrectInput}) => {
   const [input, setInput] = useState('')
+  const handleFetch = (e) => {
+    e.preventDefault()
+    getCityName(input)
+  }
 
   return (
     <header>
         <img src="/icon1.png" alt="weather app" />
         <p>Weather App</p>
-        <input onChange={(e) => {setInput(e.target.value)}} type="text" placeholder='Enter a city'/>
-        <button onClick={() => getCityName(input)}>Search</button>
+        <form onSubmit={handleFetch}>
+          <input className={incorrectInput ? 'incorrect-input' : ''} onChange={(e) => {setInput(e.target.value)}} type="text" placeholder='Enter a city'/>
+          <button type='submit'>Search</button>
+        </form>
+        
     </header>
   )
 }
